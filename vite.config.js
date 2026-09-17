ecbb57af5666a96e4f2661d0a757543cc4864424
Manuella-Fernandes <178603951+FernandesManu@users.noreply.github.com>
2026-09-17 13:26:09 +0000

    fix: use root base on vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/3D-Dev-Portfolio/",
+  base: process.env.GITHUB_ACTIONS ? "/3D-Dev-Portfolio/" : "/",
   plugins: [react(), tailwindcss()],
 });
