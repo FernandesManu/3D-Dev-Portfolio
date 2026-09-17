439ba76441e74925e68c76f3f1895d136ba3732e
Manuella-Fernandes <178603951+FernandesManu@users.noreply.github.com>
2026-09-17 13:23:59 +0000

    chore: configure github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/3D-Dev-Portfolio/",
   plugins: [react(), tailwindcss()],
 });
